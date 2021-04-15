535ab2c25f79ed3ed281db4863d372a3d23800c3
xioren <xioren00+github@protonmail.com>
2021-04-15 15:20:35 -0700

    add back guessMime and guessExtension

--- filetype/filetype.nim
+++ filetype/filetype.nim
@@ -4,3 +4,11 @@ import match, helpers
 
 proc guess*(path: string): tuple =
   match(path, allMatchers)
+
+
+proc guessMime*(path: string): string =
+  match(path, allMatchers).mime
+
+
+proc guessExtension*(path: string): string =
+  match(path, allMatchers).extension
