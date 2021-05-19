9ec4bb427c4a49c2f2a5761702651c585d7fe7fd
xioren <xioren00+github@protonmail.com>
2021-05-18 19:56:55 -0700

    optimize file i/o

--- filetype/utils.nim
+++ filetype/utils.nim
@@ -6,16 +6,16 @@ var f: File
 
 
 proc getSignatureBytes*(path: string): string =
-  # QUESTION: does checking filesize first add much to runtime?
-  if getFileSize(path) == 0:
-    return ""
-  result = newString(signatureBytes)
+  if unlikely(getFileSize(path) == 0):
+    discard
+  else:
+    result = newString(signatureBytes)
 
-  if f.open(path):
-    try:
-      let i = f.readChars(result, 0, signatureBytes)
-      result.setLen(i)
-    except OSError:
-      echo "error reading: ", path
-    finally:
-      f.close()
+    if f.open(path):
+      try:
+        let i = f.readChars(result, 0, signatureBytes)
+        result.setLen(i)
+      except OSError:
+        echo "error reading: ", path
+      finally:
+        f.close()
